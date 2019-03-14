f2218b942ac840de27bc031c19707b3a5304898e
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-03-14 20:23:56 +0200

    Design news table & Create its model

--- database/migrations/2019_03_14_181729_create_categories_table.php
+++ database/migrations/2019_03_14_181729_create_categories_table.php
@@ -16,6 +16,9 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('slug');
+
+            $table->unique(['name', 'slug']);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2019_03_14_181957_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('main_photo');
+            $table->integer('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->longText('body');
+            $table->text('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
