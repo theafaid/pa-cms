ef1750a4fa3bf595df5f65bbf17b6cc515fddf19
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-03-14 20:16:53 +0200

    Design settings table & Create its model

---
+++ database/migrations/2019_03_14_181349_create_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('site_name');
+            $table->string('site_email')->nullable();
+            $table->string('site_keywords')->nullable();
+            $table->string('site_description')->nullable();
+            $table->boolean('site_status')->default(true);
+            $table->string('site_maintenance_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
