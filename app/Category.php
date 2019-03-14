cf023c8a6f43525b57b9dbe0bf70eeea4a4541c9
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-03-15 00:00:50 +0200

    Complete storing a new category

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Validate the slug attribute for the cateogry
+     * it should be unique
+     * @param $title
+     */
+    public function setSlugAttribute($title){
+        if(static::whereSlug($slug = Str::slug($title))->exists()){
+            $slug = $slug . time();
+        }
+        $this->attributes['slug'] = $slug;
+    }
 }
--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -10,7 +10,6 @@ class CategoriesController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
@@ -23,23 +22,33 @@ class CategoriesController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('admin.categories.create', [
+            'title' => 'Create New Category',
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $data = request()->validate([
+            'name' => 'required|string|max:255|unique:categories,name'
+        ]);
+
+        Category::create([
+            'name'       => $data['name'],
+            'creator_id' => auth()->id(),
+            'slug'       => $data['name']
+        ]);
+
+        return redirect()->route('categories.index')
+            ->with('success', 'Category Created Successfully');
     }
 
     /**
